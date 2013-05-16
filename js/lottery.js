eede12d54638d3929e012d777bba30887c4dd8f1
zhangyoumin <zhangyoumin@localhost.(none)>
2013-05-16 15:38:38 +0800

    second

--- js/lottery.js
+++ js/lottery.js
@@ -33,15 +33,12 @@ Lottery.prototype.initData = function(data) {
   }
   this.data = data;
   this.lo1Index = 0;
-  this.lo2Index = 0;
   this.lo1U = new Array();
-  this.lo2U = new Array();
   this.intervalA = new Array();
   var ol = document.getElementById('ollist');
   ol.innerHTML = '';
   for(var i=0; i < uArray.length; i++) {
-    if (i % 2 == 0) this.lo1U.push(uArray[i].trim());
-    else this.lo2U.push(uArray[i].trim());
+    this.lo1U.push(uArray[i].trim())
     var li = document.createElement('li');
     var txt = document.createTextNode(uArray[i]);
     li.appendChild(txt);
@@ -50,22 +47,15 @@ Lottery.prototype.initData = function(data) {
 };
 Lottery.prototype.start = function() {
   if (this.nonEmpty(this.lo1U) && this.lo1U.length>0) this.slide(document.getElementById('lo1'));
-  if (this.nonEmpty(this.lo2U) && this.lo2U.length>0) this.slide(document.getElementById('lo2'));
 };
 Lottery.prototype.slide = function(loC) {
   var This = this;
   this.intervalA.push(window.setInterval(function(){
     loC.innerHTML = '';
     var u = '';
-    if (loC.id == 'lo1') {
-      This.lo1Index ++;
-      if (This.lo1Index >= This.lo1U.length) This.lo1Index = 0;
-      u = This.lo1U[This.lo1Index];
-    } else {
-      This.lo2Index ++;
-      if (This.lo2Index >= This.lo2U.length) This.lo2Index = 0;
-      u = This.lo2U[This.lo2Index];
-    }
+    This.lo1Index ++;
+    if (This.lo1Index >= This.lo1U.length) This.lo1Index = 0;
+    u = This.lo1U[This.lo1Index];
     if (u != undefined) {
       var div = document.createElement('div');
       var txt = document.createTextNode(u);
@@ -73,7 +63,7 @@ Lottery.prototype.slide = function(loC) {
       div.className = 'sliding';
       loC.appendChild(div);
     }
-  }, 500));
+  }, 100));
 };
 Lottery.prototype.clearInterval = function() {
   if (this.nonEmpty(this.intervalA) && this.intervalA.length>0) {
@@ -85,16 +75,10 @@ Lottery.prototype.clearInterval = function() {
 Lottery.prototype.stop = function() {
   this.clearInterval();
   var lo1 = document.getElementById('lo1').childNodes.item(0);
-  var lo2 = document.getElementById('lo2').childNodes.item(0);
   if (this.nonEmpty(lo1)) {
     lo1.className = 'item';
     this.remove(this.lo1U, lo1.innerHTML.trim());
     this.addWinner(lo1.innerHTML.trim());
-  }
-  if (this.nonEmpty(lo2)) {
-    lo2.className = 'item';
-    this.remove(this.lo2U, lo2.innerHTML.trim());
-    this.addWinner(lo2.innerHTML.trim());
   }
 };
 Lottery.prototype.addWinner = function(winner) {
